02b08650362cfee91378992422ec541062e99d1a
ben9lta <33951956+ben9lta@users.noreply.github.com>
2019-02-26 22:41:01 +0300

    Update sw.js

--- timetable/sw.js
+++ timetable/sw.js
@@ -1,47 +1,4 @@
-// addEventListener('install', installEvent => {
-//     installEvent.waitUntil(
-//       caches.open('timetable')
-//       .then( timeTable => {
-//         timeTable.addAll([
-//          'https://ben9lta.github.io/timetable/offline.html',
-//          'https://ben9lta.github.io/timetable/main.css',
-//          'https://ben9lta.github.io/timetable/weeks.js',
-//          'https://ben9lta.github.io/timetable/img/icons/favicon/favicon-16x16.png'
-//         ]); // конец addAll
-//       }) // конец open.then
-//     ); // конец waitUntil
-//   }); // конец addEventListener
-
-//   // Всегда, когда файл запрашивается
-// addEventListener('fetch', fetchEvent => {
-//   const request = fetchEvent.request;
-//   fetchEvent.respondWith(
-//     // Сначала попытка запросить его из Сети
-//     fetch(request)
-//     .then( responseFromFetch => {
-//       return responseFromFetch;
-//     }) // конец fetch.then
-//     // Если не сработало, то...
-//     .catch( fetchError => {
-//       // пытаемся найти в кеше
-//       caches.match(request)
-//       .then( responseFromCache => {
-//         if (responseFromCache) {
-//          return responseFromCache;
-//        // если не сработало и...
-//        } else {
-//          // это запрос к веб-странице, то...
-//          if (request.headers.get('Accept').includes('text/html')) {
-//            // покажите вашу офлайн-страницу
-//            return caches.match('https://ben9lta.github.io/timetable/offline.html');
-//          } // 1конец if
-//        } // конец if/else
-//      }) // конец match.then
-//    }) // конец fetch.catch
-//   ); // конец respondWith
-// }); // конец addEventListener
-
-const CACHE_NAME = 'timestamp-v2';
+const CACHE_NAME = 'timestamp-v1';
 const CACHE_URLS = [
   '/offline.html',
   '/main.css',
@@ -49,54 +6,94 @@ const CACHE_URLS = [
   '/img/icons/favicon/favicon-16x16.png',
 ];
 
-self.addEventListener('install', function (event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function (cache) {
-        return cache.addAll(CACHE_URLS);
-      })
-  );
-});
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName.startsWith('pages-cache-') && staticCacheName !== cacheName) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+addEventListener('install', installEvent => {
+    installEvent.waitUntil(
+      caches.open(CACHE_NAME)
+      .then( timeTable => {
+        timeTable.addAll(CACHE_URLS); // конец addAll
+      }) // конец open.then
+    ); // конец waitUntil
+  }); // конец addEventListener
 
-self.addEventListener('fetch', function (event) {
-  console.log('Fetch event for ', event.request.url);
-  var response;
-  event.respondWith(caches.match(event.request).then(function (response) {
-    if (response) {
-      console.log('Found ', event.request.url, ' in cache');
-      return response;
-    }
-    console.log('Network request for ', event.request.url);
-    return fetch(event.request).then(function (response) {
-      if (response.status === 404) {
-        return caches.match('main.css')
-      }
-      return caches.open(CACHE_URLS).then(function (cache) {
-        cache.put(event.request.url, response.clone());
-        return response;
-      });
-    });
-  }).catch(function (error) {
-    console.log('Error, ', error);
-    return caches.match('offline.html');
-  })
-  );
-});
+  // Всегда, когда файл запрашивается
+addEventListener('fetch', fetchEvent => {
+  const request = fetchEvent.request;
+  fetchEvent.respondWith(
+    // Сначала попытка запросить его из Сети
+    fetch(request)
+    .then( responseFromFetch => {
+      return responseFromFetch;
+    }) // конец fetch.then
+    // Если не сработало, то...
+    .catch( fetchError => {
+      // пытаемся найти в кеше
+      caches.match(request)
+      .then( responseFromCache => {
+        if (responseFromCache) {
+         return responseFromCache;
+       // если не сработало и...
+       } else {
+         // это запрос к веб-странице, то...
+         if (request.headers.get('accept').includes('text/html')) {
+           // покажите вашу офлайн-страницу
+           return caches.match('/offline.html');
+         } // 1конец if
+       } // конец if/else
+     }) // конец match.then
+   }) // конец fetch.catch
+  ); // конец respondWith
+}); // конец addEventListener
+
+
+// self.addEventListener('install', function (event) {
+//   console.log('[Service Worker] Installing Service Worker ...', event);
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function (cache) {
+//         return cache.addAll(CACHE_URLS);
+//       })
+//   );
+// });
+
+// self.addEventListener('activate', function(event) {
+//   event.waitUntil(
+//     caches.keys().then(function(cacheNames) {
+//       return Promise.all(
+//         cacheNames.map(function(cacheName) {
+//           if (cacheName.startsWith('pages-cache-') && staticCacheName !== cacheName) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
+
+// self.addEventListener('fetch', function (event) {
+//   console.log('Fetch event for ', event.request.url);
+//   var response;
+//   event.respondWith(caches.match(event.request).then(function (response) {
+//     if (response) {
+//       console.log('Found ', event.request.url, ' in cache');
+//       return response;
+//     }
+//     console.log('Network request for ', event.request.url);
+//     return fetch(event.request).then(function (response) {
+//       if (response.status === 404) {
+//         return caches.match('main.css')
+//       }
+//       return caches.open(CACHE_URLS).then(function (cache) {
+//         cache.put(event.request.url, response.clone());
+//         return response;
+//       });
+//     });
+//   }).catch(function (error) {
+//     console.log('Error, ', error);
+//     return caches.match('offline.html');
+//   })
+//   );
+// });
     // else
     // {
     //   return fetch(event.request).catch(function (err) {
