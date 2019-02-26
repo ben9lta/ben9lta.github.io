4c440bd6b7c4146d5067a3b0d009e5725ea33d5b
ben9lta <33951956+ben9lta@users.noreply.github.com>
2019-02-26 23:47:24 +0300

    Update sw.js

--- timetable/sw.js
+++ timetable/sw.js
@@ -157,7 +157,8 @@ self.addEventListener('fetch', (event) => {
     // try the cache
     caches.match(event.request).then((cachedResponse) =>
       // fall back to network
-      cachedResponse || fetch(event.request)
+      console.log(fetch(event.request)),
+      cachedResponse //|| fetch(event.request)
     ).catch(() =>
       // if both fail, show a generic fallback:
       caches.match('/offline.html')
