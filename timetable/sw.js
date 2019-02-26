e48b175905025c8c60743653c7f2eb0c00c79658
ben9lta <33951956+ben9lta@users.noreply.github.com>
2019-02-27 00:14:07 +0300

    update

--- timetable/sw.js
+++ timetable/sw.js
@@ -95,17 +95,17 @@ const CACHE_URLS = [
 //   })
 //   );
 // });
-    // else
-    // {
-    //   return fetch(event.request).catch(function (err) {
-    //     return caches.open(CACHE_NAME).then(function(cache){
-    //       if(event.request.headers.get('accept').includes('text/html')) {
-    //         return cache.match('/offline.html');
-    //       }
-    //     });
-    //   });
+// else
+// {
+//   return fetch(event.request).catch(function (err) {
+//     return caches.open(CACHE_NAME).then(function(cache){
+//       if(event.request.headers.get('accept').includes('text/html')) {
+//         return cache.match('/offline.html');
+//       }
+//     });
+//   });
 
-    // }
+// }
 
 //   }).then(function(r) {
 //     response = r;
@@ -149,43 +149,43 @@ self.addEventListener('activate', (event) => {
           return null;
         })
       )
-  ));
+    ));
 });
 
 var MAX_AGE = 86400000;
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-      // ищем запрошенный ресурс среди закэшированных
-      caches.match(event.request).then(function(cachedResponse) {
-          var lastModified, fetchRequest;
-          // если ресурс есть в кэше
-          if (cachedResponse) {
-              // получаем дату последнего обновления
-              lastModified = new Date(cachedResponse.headers.get('last-modified'));
-              // и если мы считаем ресурс устаревшим
-              if (lastModified && (Date.now() - lastModified.getTime()) > MAX_AGE) {
-                  fetchRequest = event.request.clone();
-                  // создаём новый запрос
-                  return fetch(fetchRequest).then(function(response) {
-                      // при неудаче всегда можно выдать ресурс из кэша
-                      if (!response || response.status !== 200) {
-                          return cachedResponse;
-                      }
-                      // обновляем кэш
-                      caches.open(CACHE_NAME).then(function(cache) {
-                          cache.put(event.request, response.clone());
-                      });
-                      // возвращаем свежий ресурс
-                      return response;
-                  }).catch(function() {
-                      return cachedResponse;
-                  });
-              }
+    // ищем запрошенный ресурс среди закэшированных
+    caches.match(event.request).then(function (cachedResponse) {
+      var lastModified, fetchRequest;
+      // если ресурс есть в кэше
+      if (cachedResponse) {
+        // получаем дату последнего обновления
+        lastModified = new Date(cachedResponse.headers.get('last-modified'));
+        // и если мы считаем ресурс устаревшим
+        if (lastModified && (Date.now() - lastModified.getTime()) > MAX_AGE) {
+          fetchRequest = event.request.clone();
+          // создаём новый запрос
+          return fetch(fetchRequest).then(function (response) {
+            // при неудаче всегда можно выдать ресурс из кэша
+            if (!response || response.status !== 200) {
               return cachedResponse;
-          }
-          // запрашиваем из сети как обычно
-          return fetch(event.request);
-      })
+            }
+            // обновляем кэш
+            caches.open(CACHE_NAME).then(function (cache) {
+              cache.put(event.request, response.clone());
+            });
+            // возвращаем свежий ресурс
+            return response;
+          }).catch(function () {
+            return cachedResponse;
+          });
+        }
+        return cachedResponse;
+      }
+      // запрашиваем из сети как обычно
+      return fetch(event.request);
+    })
   );
 });
