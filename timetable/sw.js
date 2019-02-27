86d24481ccb058a7683a2856ed82231e8d5e6be0
ben9lta <33951956+ben9lta@users.noreply.github.com>
2019-02-27 13:05:05 +0300

    update version

--- timetable/sw.js
+++ timetable/sw.js
@@ -1,5 +1,5 @@
 //const CACHE_NAME = 'v11';
-const CACHE_NAME = 'v3';
+const CACHE_NAME = 'v4';
 const CACHE_URLS = [
   '/timetable/offline.html',
   '/timetable/main.css',
@@ -22,7 +22,7 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['v1', 'v2'];
+  var cacheWhitelist = ['v3'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
