83be5c8f3a4b61f3396f074814f270ae8077db65
ben9lta <33951956+ben9lta@users.noreply.github.com>
2020-03-10 15:20:47 +0300

    Update sw.js

--- timetable/sw.js
+++ timetable/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'v36';
+const CACHE_NAME = 'v37';
 const CACHE_URLS = [
   '/timetable/index.html',
   '/timetable/main.css',
@@ -21,7 +21,7 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['v35'];
+  var cacheWhitelist = ['v36'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
